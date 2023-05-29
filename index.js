0c0270beaaa26f26042a6fc178e6040cafee79e9
Erick <adolfo.solis.is@unipolidgo.edu.mx>
2023-05-29 11:00:28 -0600

    2

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-import fs from 'fs';
-import readline from 'readline';
-import { tokenPatterns } from './table.js';
-import { tokens, States, Finals } from './table.js';
+import fs from "fs";
+import readline from "readline";
+import { tokenPatterns } from "./table.js";
+import { tokens, States, Finals } from "./table.js";
 
-const inputFile = 'file.txt';
-const outputFile = 'output.txt';
-const errorFile = 'error.txt';
+const inputFile = "file.txt";
+const outputFile = "output.txt";
+const errorFile = "error.txt";
 
 // Create a readable stream to read the input file line by line
-const readStream = fs.createReadStream(inputFile, 'utf8');
+const readStream = fs.createReadStream(inputFile, "utf8");
 
 // Create a writable stream to write the results to the output file
-const writeStream = fs.createWriteStream(outputFile, 'utf8');
+const writeStream = fs.createWriteStream(outputFile, "utf8");
 
 // Create a writable stream to write the results to the error file
-const errorStream = fs.createWriteStream(errorFile, 'utf8');
+const errorStream = fs.createWriteStream(errorFile, "utf8");
 
 // Create an interface to read lines from the input stream
 const rl = readline.createInterface({
   input: readStream,
-  crlfDelay: Infinity
+  crlfDelay: Infinity,
 });
 
 // Tokenize the source code line by line
 const analyzeLine = (line) => {
   const results = [];
-  let prev = '';
+  let prev = "";
   let source = line;
   let totalCount = line.length;
   let count = 0;
@@ -66,43 +66,100 @@ const analyzeLine = (line) => {
 
 // Process each line of the input file
 let lineCount = 0;
-rl.on('line', (line) => {
+let curr = "";
+let state = 0;
+let rsl = [];
+rl.on("line", (line) => {
+  const analyzeChar = (char, position) => {
+    const currState = States[state];
+    if (currState.moves) {
+      for (const key in currState.moves) {
+        const compare = RegExp(key);
+        const match = curr.match(compare);
+        if (match) {
+          state = currState.moves[key];
+          curr.concat(char);
+        }
+      }
+    } else if (currState.will === "end") {
+      rsl.push({ type: Finals[state], value: curr });
+      state = 0;
+      curr = "";
+    } else {
+      throw new Error(`Invalid character: ${char}, at position ${position}`);
+    }
+  };
+
   try {
     const results = analyzeLine(line);
+    const chars = line.split("");
+
+    for (let index = 0; index < chars.length; index++) {
+      const char = chars[index];
+      const pos = index + 1;
+      analyzeChar(char, pos);
+    }
 
     lineCount++;
     // Write the results to the output file
     writeStream.write(
-      results.map((token) => JSON.stringify(token)).join('\n') + '\n'
+      results.map((token) => JSON.stringify(token)).join("\n") + "\n"
     );
   } catch (error) {
     lineCount++;
-    errorStream.write('Error on line ' + lineCount + ' -> ' + error);
-    console.error('Error on line ' + lineCount + ' -> ' + error);
+    errorStream.write("Error on line " + lineCount + " -> " + error);
+    console.error("Error on line " + lineCount + " -> " + error);
   }
 });
 
 // Close the write stream when all lines have been processed
-rl.on('close', () => {
-  console.log('Finished');
+rl.on("close", () => {
+  console.log("Finished");
   writeStream.end();
 });
 
-function verify(States, Finals, line) {
+/* function verify(line) {
   let results = { data: [], error: [] };
-  function recursiveCheck(state, nline, results, carries) {
-    const curr = line.slice(0, 1);
-    const nextLine = line.slice(1, -1);
-    for (const key in States.moves) {
-      let compare = '';
-      if (key.length > 1) {
-        if (curr === key) {
-          recursiveCheck(States.moves[key], nextLine, results, carries);
-        }
+
+  const recursiveCheck = (state, nline, results, carries, currCheck) => {
+    if (nline.length === 0) {
+      if (carries) {
+        return {
+          state: state,
+          data: results,
+          carries: carries,
+          curr: currCheck,
+        };
       } else {
+        return {
+          data: results,
+        };
       }
     }
-  }
+    const currState = States[state];
+    const curr = line.slice(0, 1);
+    const nextLine = line.slice(1, -1);
+    if (currState.moves) {
+      for (const key in currState.moves) {
+        const compare = RegExp(key);
+        const match = curr.match(compare);
+        if (match) {
+          const willCarry = currState?.will === "carries" ? true : false;
+          const nextCheck = currCheck.concat(curr);
+          return recursiveCheck(
+            currState.moves[key],
+            nextLine,
+            results,
+            willCarry,
+            nextCheck
+          );
+        }
+      }
+    } else if (currState?.will === "end") {
+      results.push({ type: Finals[state], value: currCheck });
+      recursiveCheck(currState.moves[key], nextLine, results, false, "");
+    }
+  };
   try {
     results = recursiveCheck(0, line, results, false);
     return;
@@ -111,3 +168,4 @@ function verify(States, Finals, line) {
     throw new Error(results);
   }
 }
+ */
--- table.js
+++ table.js
@@ -22,221 +22,224 @@ export const tokenPatterns = {
   colon: /^:/,
   whitespace: /^\s+/,
   tab: /^\t+/,
-  break: /^\n+/
+  break: /^\n+/,
 };
 
 export const tokenCarryPatterns = {
   integer: /^\d+/,
   string: /^(\n+)?.*\'/,
-  comment: /^(\n+)?.*\*\//
+  comment: /^(\n+)?.*\*\//,
 };
 
 export const tokens = {
-  identifier: 'IDENTIFIER',
-  integer: 'INTEGER',
-  float: 'FLOAT',
-  string: 'STRING',
-  comment: 'COMMENT',
-  plus: 'PLUS',
-  minus: 'MINUS',
-  div: 'DIVISION',
-  multiply: 'MULTIPLY',
-  assign: 'ASSIGN',
-  equal: 'EQUAL',
-  and: 'AND',
-  or: 'OR',
-  different: 'DIFFERENT',
-  bigequ: 'MORE OR EQUAL',
-  minequ: 'LESS OR EQUAL',
-  big: 'MORE',
-  min: 'LESS',
-  no: 'NEGATION',
-  semicolon: 'SEMICOLON',
-  colon: 'COLON',
-  whitespace: 'WHITESPACE',
-  tab: 'TAB',
-  break: 'LINE-BREAK'
+  identifier: "IDENTIFIER",
+  integer: "INTEGER",
+  float: "FLOAT",
+  string: "STRING",
+  comment: "COMMENT",
+  plus: "PLUS",
+  minus: "MINUS",
+  div: "DIVISION",
+  multiply: "MULTIPLY",
+  assign: "ASSIGN",
+  equal: "EQUAL",
+  and: "AND",
+  or: "OR",
+  different: "DIFFERENT",
+  bigequ: "MORE OR EQUAL",
+  minequ: "LESS OR EQUAL",
+  big: "MORE",
+  min: "LESS",
+  no: "NEGATION",
+  semicolon: "SEMICOLON",
+  colon: "COLON",
+  whitespace: "WHITESPACE",
+  tab: "TAB",
+  break: "LINE-BREAK",
 };
 
 export const States = {
   0: {
     moves: {
-      $: 1,
-      '^d+': 4,
-      "'": 6,
-      '/': 9,
-      '*': 22,
-      '+': 23,
-      '=': 14,
-      '-': 24,
-      '>': 16,
-      '<': 18,
-      '!': 20,
-      '|': 25,
-      '&': 27,
-      ';': 38,
-      ':': 39
-    }
+      '$': 1,
+      "\\d": 4,
+      "\\'": 7,
+      "\\/": 10,
+      "\\*": 23,
+      "\\+": 24,
+      "=": 15,
+      "-": 25,
+      ">": 17,
+      "<": 19,
+      "!": 21,
+      "\\|": 26,
+      "&": 28,
+      "{": 30,
+      "\\[": 33,
+      "(": 36,
+      ";": 39,
+      ":": 40,
+    },
   },
   1: {
-    moves: { '/^$[a-zA-Z_]w*/': 2 }
+    moves: { "^[a-zA-Z_]": 2 },
   },
   2: {
-    moves: { '^\\$[a-zA-Z_]\\w*': 2, '^d+': 3 },
-    will: 'end'
+    moves: { "^[a-zA-Z_]*": 2, "^d": 3 },
+    will: "end",
   },
   3: {
-    moves: { '^\\$[a-zA-Z_]\\w*': 2, '^d+': 3 },
-    will: 'end'
+    moves: { "^[a-zA-Z_]": 2, "^d": 3 },
+    will: "end",
   },
   4: {
-    moves: { '^d+': 4, '.': 5 },
-    will: 'end'
+    moves: { "^d": 4, "\\.": 5 },
+    will: "end",
   },
   5: {
-    moves: { '^d+': 6 }
+    moves: { "^d": 6 },
   },
   6: {
-    moves: { '^d+': 6 },
-    will: 'end'
+    moves: { "^d": 6 },
+    will: "end",
   },
   7: {
-    moves: { '^.*': 8 },
-    will: 'carry'
+    moves: { "^.": 8 },
+    will: "carry",
   },
   8: {
-    moves: { '^.*': 8, "'": 9 }
+    moves: { "^.": 8, "'": 9 },
   },
   9: {
-    will: 'end'
+    will: "end",
   },
   10: {
-    moves: { '*': 11 },
-    will: 'end'
+    moves: { "\\*": 11 },
+    will: "end",
   },
   11: {
-    moves: { '^.*': 12 },
-    will: 'carry'
+    moves: { "^.": 12 },
+    will: "carry",
   },
   12: {
-    moves: { '^.*': 12, '*': 13 }
+    moves: { "^[.\\s\\t]": 12, "*": 13 },
   },
   13: {
-    moves: { '/': 14 }
+    moves: { "\\/": 14 },
   },
   14: {
-    will: 'end'
+    will: "end",
   },
   15: {
-    moves: { '=': 16 },
-    will: 'end'
+    moves: { "=": 16 },
+    will: "end",
   },
   16: {
-    will: 'end'
+    will: "end",
   },
   17: {
-    moves: { '=': 18 },
-    will: 'end'
+    moves: { "=": 18 },
+    will: "end",
   },
   18: {
-    will: 'end'
+    will: "end",
   },
   19: {
-    moves: { '=': 20 },
-    will: 'end'
+    moves: { "=": 20 },
+    will: "end",
   },
   20: {
-    will: 'end'
+    will: "end",
   },
   21: {
-    moves: { '=': 22 },
-    will: 'end'
+    moves: { "=": 22 },
+    will: "end",
   },
   22: {
-    will: 'end'
+    will: "end",
   },
   23: {
-    will: 'end'
+    will: "end",
   },
   24: {
-    will: 'end'
+    will: "end",
   },
   25: {
-    will: 'end'
+    will: "end",
   },
   26: {
-    moves: { '|': 27 }
+    moves: { "\\|": 27 },
   },
   27: {
-    will: 'end'
+    will: "end",
   },
   28: {
-    moves: { '&': 29 }
+    moves: { "&": 29 },
   },
   29: {
-    will: 'end'
+    will: "end",
   },
   30: {
-    moves: { '^.*': 31, '}': 32 },
-    will: 'carry'
+    moves: { "^.": 31, "}": 32 },
+    will: "carry",
   },
   31: {
-    moves: { '^.*': 31, '}': 32 }
+    moves: { "^.": 31, "}": 32 },
   },
   32: {
-    will: 'end'
+    will: "end",
   },
   33: {
-    moves: { '^.*': 34, '}': 35 },
-    will: 'carry'
+    moves: { "^.": 34, "\\]": 35 },
+    will: "carry",
   },
   34: {
-    moves: { '^.*': 34, '}': 35 }
+    moves: { "^.": 34, "\\]": 35 },
   },
   35: {
-    will: 'end'
+    will: "end",
   },
   36: {
-    moves: { '^.*': 37, '}': 38 },
-    will: 'carry'
+    moves: { "^.": 37, "\\)": 38 },
+    will: "carry",
   },
   37: {
-    moves: { '^.*': 37, '}': 38 }
+    moves: { "^.": 37, "\\)": 38 },
   },
   38: {
-    will: 'end'
+    will: "end",
   },
   39: {
-    will: 'end'
+    will: "end",
   },
   40: {
-    will: 'end'
-  }
+    will: "end",
+  },
 };
 
 export const Finals = {
-  2: 'IDENTIFIER',
-  3: 'IDENTIFIER',
-  4: 'INTEGER',
-  6: 'FLOAT',
-  9: 'STRING',
-  14: 'COMMENT',
-  15: 'EQUAK',
-  16: 'IS EQUALS',
-  17: 'MORE',
-  18: 'MORE OR EQUAL',
-  19: 'LESSER',
-  20: 'LESSER OR EQUAL',
-  21: 'NEGATIVE',
-  22: 'DIFFERENT',
-  23: 'MULTIPLY',
-  24: 'PLUS',
-  25: 'MINUS',
-  27: 'OR',
-  29: 'AND',
-  32: 'BRACKETS',
-  35: '[sdda]',
-  38: 'PARENTASIS',
-  39: 'SEMI-COLON',
-  40: 'COLON'
+  2: "IDENTIFIER",
+  3: "IDENTIFIER",
+  4: "INTEGER",
+  6: "FLOAT",
+  9: "STRING",
+  14: "COMMENT",
+  15: "EQUAK",
+  16: "IS EQUALS",
+  17: "MORE",
+  18: "MORE OR EQUAL",
+  19: "LESSER",
+  20: "LESSER OR EQUAL",
+  21: "NEGATIVE",
+  22: "DIFFERENT",
+  23: "MULTIPLY",
+  24: "PLUS",
+  25: "MINUS",
+  27: "OR",
+  29: "AND",
+  32: "BRACKETS",
+  35: "[sdda]",
+  38: "PARENTASIS",
+  39: "SEMI-COLON",
+  40: "COLON",
 };
