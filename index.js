cc6406f1dff27ffc71ad0859d03d9cde96dbf79b
Erick <adolfo.solis.is@unipolidgo.edu.mx>
2023-06-30 01:24:48 -0600

    11

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-import fs from "fs";
-import readline from "readline";
-import { States, Finals, Reserved } from "./table.js";
+import fs from 'fs';
+import readline from 'readline';
+import { States, Finals, Reserved } from './table.js';
 
-const inputFile = "file.txt";
-const outputFile = "output.txt";
-const errorFile = "error.txt";
+const inputFile = 'file.txt';
+const outputFile = 'output.txt';
+const errorFile = 'error.txt';
 
 // Create a readable stream to read the input file line by line
-const readStream = fs.createReadStream(inputFile, "utf8");
+const readStream = fs.createReadStream(inputFile, 'utf8');
 
 // Create a writable stream to write the results to the output file
-const writeStream = fs.createWriteStream(outputFile, "utf8");
+const writeStream = fs.createWriteStream(outputFile, 'utf8');
 
 // Create a writable stream to write the results to the error file
-const errorStream = fs.createWriteStream(errorFile, "utf8");
+const errorStream = fs.createWriteStream(errorFile, 'utf8');
 
 // Create an interface to read lines from the input stream
 const rl = readline.createInterface({
   input: readStream,
-  crlfDelay: Infinity,
+  crlfDelay: Infinity
 });
 
 // Process each line of the input file
 let lineCount = 0;
-let curr = "";
+let curr = '';
 let state = 0;
 let carry = false;
 const rsl = [];
@@ -34,46 +34,46 @@ const analyzeChar = (char, position) => {
   if (currState.moves) {
     for (const key in currState.moves) {
       const compare = RegExp(key);
-      console.log("Comparison -> ", compare, " to: <", char, ">");
-      console.log("current token -> ", curr);
+      console.log('Comparison -> ', compare, ' to: <', char, '>');
+      console.log('current token -> ', curr);
       const match = char.match(compare);
-      console.log("Matched? -> ", match);
+      console.log('Matched? -> ', match);
       if (match) {
-        if (currState.will === "carry") {
+        if (currState.will === 'carry') {
           carry = true;
         }
         state = currState.moves[key];
         matched = true;
         curr = curr ? char : curr + char;
-        console.log("new <", curr, ">");
+        console.log('new <', curr, '>');
         break;
       }
     }
-  } else if (currState.will === "end") {
+  } else if (currState.will === 'end') {
     rsl.push({ type: Finals[state], value: curr });
     carry = false;
     matched = true;
-    console.log("--> final -> ", curr);
+    console.log('--> final -> ', curr);
     state = 0;
-    curr = "";
+    curr = '';
     return true;
-  } else if (currState.will === "reserved") {
+  } else if (currState.will === 'reserved') {
     if (Object.keys(Reserved).includes(curr[0])) {
       for (let index = 0; index < Reserved[curr[0]].length; index++) {
         const word = Reserved[curr[0]][index];
         if (word.includes(curr)) {
           if (word.length === curr.length) {
-            console.log("Reserved -> ", word, " to: <", curr, ">");
-            rsl.push({ type: "RESERVED", value: curr });
+            console.log('Reserved -> ', word, ' to: <', curr, '>');
+            rsl.push({ type: 'RESERVED', value: curr });
             carry = false;
             matched = true;
-            console.log("--> final -> ", curr);
+            console.log('--> final -> ', curr);
             state = 0;
-            curr = "";
+            curr = '';
             return true;
           } else {
             curr = curr + char;
-            console.log("Reserved -> ", word, " to: <", curr, ">");
+            console.log('Reserved -> ', word, ' to: <', curr, '>');
             matched = true;
             break;
           }
@@ -82,7 +82,7 @@ const analyzeChar = (char, position) => {
     }
   }
   if (!matched) {
-    if (currState.will === "end") {
+    if (currState.will === 'end') {
       if (currState.predates) {
         if (Finals[currState.predates] === rsl[rsl.length - 1].type) {
           rsl.pop();
@@ -91,9 +91,9 @@ const analyzeChar = (char, position) => {
       rsl.push({ type: Finals[state], value: curr });
       carry = false;
       matched = true;
-      console.log("--> final -> ", curr);
+      console.log('--> final -> ', curr);
       state = 0;
-      curr = "";
+      curr = '';
       return true;
     } else {
       throw new Error(`Invalid character: ${char}, at position ${position}`);
@@ -101,19 +101,19 @@ const analyzeChar = (char, position) => {
   }
 };
 
-rl.on("line", (line) => {
+rl.on('line', (line) => {
   try {
-    const chars = line.split("");
+    const chars = line.split('');
     console.log(chars, chars.length);
     for (let index = 0; index <= chars.length; index++) {
       let char;
       if (index < chars.length) {
         char = chars[index];
       } else {
-        char = " ";
+        char = ' ';
       }
       const pos = index + 1;
-      console.log("   State -> ", state, " - Pos -> ", index);
+      console.log('   State -> ', state, ' - Pos -> ', index);
       const n = analyzeChar(char, pos);
       if (n) {
         index--;
@@ -122,34 +122,34 @@ rl.on("line", (line) => {
     lineCount++;
     // Write the results to the output file
     writeStream.write(
-      rsl.map((token) => JSON.stringify(token)).join("\n") + "\n"
+      rsl.map((token) => JSON.stringify(token)).join('\n') + '\n'
     );
   } catch (error) {
     lineCount++;
     // writes correct ones
     if (rsl) {
       writeStream.write(
-        rsl.map((token) => JSON.stringify(token)).join("\n") + "\n"
+        rsl.map((token) => JSON.stringify(token)).join('\n') + '\n'
       );
     }
     // writes error
-    errorStream.write("Error on line " + lineCount + " -> " + error);
-    console.error("Error on line " + lineCount + " -> " + error);
+    errorStream.write('Error on line ' + lineCount + ' -> ' + error);
+    console.error('Error on line ' + lineCount + ' -> ' + error);
   }
 });
 
 // Close the write stream when all lines have been processed
-rl.on("close", () => {
+rl.on('close', () => {
   if (carry) {
     console.error(
-      "Error on line " + lineCount + " -> " + curr + " was not closed"
+      'Error on line ' + lineCount + ' -> ' + curr + ' was not closed'
     );
     errorStream.write(
-      "Error on line " + lineCount + " -> " + curr + " was not closed"
+      'Error on line ' + lineCount + ' -> ' + curr + ' was not closed'
     );
   }
   console.log(rsl);
-  console.log("Finished");
+  console.log('Finished');
   writeStream.end();
   errorStream.end();
 });
--- sintax.js
+++ sintax.js
@@ -1,6 +1,121 @@
+const parsingMap = {
+  '<program>': ['program.init; <body>'],
+  '<body>': ['<declaration> <principal>', '<principal>'],
+  '<declaration>': ['var <list_id>:<types> ; <aux1>'],
+  '<aux1>': ['<list_id> : <types> ; <aux1>', 'EPSILON'],
+  '<list_id>': ['id <aux2>'],
+  '<aux2>': [', id <aux2>', 'EPSILON'],
+  '<types>': ['<standard>', '<vec>'],
+  '<standard>': ['int', 'float', 'string', 'boolean'],
+  '<principal>': ['{ <statutes> }'],
+  '<statutes>': ['<estatute> ; <statutes>'],
+  '<vec>': ['array[cte.int .. cte.int <aux3> ] of <standard>'],
+  '<aux3>': [',cte.int .. cte.int <aux3>', 'EPSILON'],
+  '<estatute>': [
+    '<asignation>',
+    '<forloop>',
+    '<whileloop>',
+    '<repeatloop>',
+    '<input>',
+    '<output>',
+    '<conditional>'
+  ],
+  '<asignation>': ['<var> = <expresion>'],
+  '<var>': ['id <aux4>'],
+  '<aux4>': ['[<expresion><aux5>]', 'EPSILON'],
+  '<aux5>': [',<expresion><aux5>', 'EPSILON'],
+  '<repeatloop>': ['repeat <statutes> until <expresion>'],
+  '<forloop>': ['for <counter> do {<statutes>}'],
+  '<counter>': ['id=<expresion> for <expresion>'],
+  '<whileloop>': ['while <expresion> do { <statutes> }'],
+  '<input>': ['read( <var> <aux6> )'],
+  '<aux6>': [', <var> <aux6>', 'EPSILON'],
+  '<output>': ['write( <var> <aux7> )'],
+  '<aux7>': [', <var> <aux7>', 'EPSILON'],
+  '<conditional>': ['if <expresion> then { <statutes> } <aux8>'],
+  '<aux8>': ['else { <statutes> } ', 'EPSILON'],
+  '<expresion>': ['<exp> <aux9>'],
+  '<aux9>': ['<relational> <exp>', 'EPSILON'],
+  '<exp>': ['<term> <aux10>'],
+  '<aux10>': ['+ <exp>', '- <exp>', '|| <exp>', 'EPSILON'],
+  '<relational>': ['==', '<', '>', '<=', '>=', '!='],
+  '<term>': ['<factor> <aux11>'],
+  '<aux11>': [
+    '*<factor>',
+    '/<factor>',
+    '%<factor>',
+    '&&<factor>',
+    'EPSILON'
+  ],
+  '<factor>': [
+    '(<expresion>)',
+    '<var>',
+    'true',
+    'false',
+    'cte.int',
+    'cte.float',
+    'cte.string',
+    '! <expresion>'
+  ]
+};
 
-
-let currSint;
-export const sintaxAnalyzer = (token) => {
-
+export function u3Grammars (sentence, grammar) {
+  const original = sentence;
+  let res = false;
+  function S (str, rule) {
+    if (!rule) {
+      rule = Object.keys(grammar)[0];
+      str = Object.keys(grammar)[0];
+    }
+    // console.log('before:' + str + rule);
+    const rIncluded = Object.keys(grammar).some((value) => str.includes(value));
+    if (rIncluded && str.length <= original.length) {
+      for (let i = 0; i < grammar[rule].length; i++) {
+        const gen = str.replace(rule, grammar[rule][i]);
+        const left = Object.keys(grammar).some((value) => gen.includes(value));
+        // console.log('::=' + gen);
+        if (left) {
+          for (let index = 0; index < Object.keys(grammar).length; index++) {
+            const element = Object.keys(grammar)[index];
+            if (gen.includes(element)) {
+              // console.log(gen, element);
+              const ver = S(gen, element);
+              if (ver) {
+                return true;
+              } else {
+                res = ver;
+              }
+            }
+          }
+        } else {
+          if (gen === original) {
+            console.log(original + ' = ' + gen);
+            res = true;
+            break;
+          } else {
+            res = false;
+            break;
+          }
+        }
+      }
+    } else {
+      // console.log(str, rule);
+      if (str === original) {
+        return true;
+      } else {
+        res = false;
+      }
+    }
+    return res;
+  }
+  const result = S();
+  return result;
 }
+
+const currSintax = '';
+const posibleProductions = [];
+export const sintaxAnalyzer = (token) => {
+  if (!posibleProductions) {
+
+  }
+};
